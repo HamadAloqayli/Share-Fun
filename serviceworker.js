c42797e553b49217891977a2ad5198a598484860
Hamad Aloqayli <38769819+HamadAloqayli@users.noreply.github.com>
2021-05-05 01:36:07 +0300

    edits on cache assets

--- serviceworker.js
+++ serviceworker.js
@@ -2,11 +2,6 @@ const CACHE_NAME = "version-1";
 const assets = [
                 '/',
                 '/index.html',
-                '/Login',
-                '/Signup',
-                '/Profile',
-                '/Groups',
-                '/Favorite',
                 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
                 'https://code.jquery.com/jquery-3.5.1.slim.min.js',
                 'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js',
